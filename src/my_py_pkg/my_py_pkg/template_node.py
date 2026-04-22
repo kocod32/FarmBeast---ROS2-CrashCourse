0b260bf2e17e0111afbb9469898f5f3d97e3504f
Niko Korošec <niko.korosec1@gmail.com>
2026-04-22 15:29:16 +0200

    add template node

---
+++ src/my_py_pkg/my_py_pkg/template_node.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3  
+  
+import rclpy  
+from rclpy.node import Node  
+  
+  
+class TemplateNode(Node):  
+    def __init__(self):  
+        super().__init__("template_node")  
+        self.counter_ = 0  
+    
+  
+def main(args=None):  
+    rclpy.init(args=args)  
+    node = TemplateNode()  
+    rclpy.spin(node)  
+    rclpy.shutdown()  
+  
+  
+if __name__ == "__main__":  
+    main()
